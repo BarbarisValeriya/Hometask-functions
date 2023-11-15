2a42ddd7c54500e4ce252a3aa3de1eb29352a690
Valeriia <borisova.valeriya.n@mail.ru>
2023-11-15 22:40:48 +0300

    задача 2 решена

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,4 +1,33 @@
 ﻿/* Задайте массив заполненный случайными трёхзначными числами. 
 Напишите программу, которая покажет количество чётных чисел в массиве.*/
 
+int[] CreateArray()
+{
+    Console.WriteLine("введите размер массива ");
+    int length = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[length];
+    Random rand = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rand.Next(99, 999);
+    }
+    return array;
+}
 
+int Task(int[] array)
+{
+    int count_number = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count_number++;
+        }
+    }
+    return count_number;
+}
+
+int[] array = CreateArray();
+int count_number = Task(array);
+
+Console.WriteLine("в массиве " + string.Join(", ", array) + " число четных чисел " + count_number);
