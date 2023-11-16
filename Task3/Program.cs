6e327f544a45e9c742889fa10b2c30c09dbe2794
Valeriia <borisova.valeriya.n@mail.ru>
2023-11-16 23:46:30 +0300

    решение с помощью reverse

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,4 +1,16 @@
 ﻿/*Напишите программу, которая перевернёт одномерный массив 
 (первый элемент станет последним, второй – предпоследним и т.д.)*/
 
+Random rand = new Random();
+int length = 10;
+int[] array = new int[length];
 
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(1, 100);
+    Console.Write($"{array[i]} ");
+}
+
+Array.Reverse(array);
+Console.WriteLine();
+Console.WriteLine(String.Join(',', array));
